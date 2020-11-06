59412edae6f8e921a7854e113b8d249a49a1158f
Kris Coolen <kris.coolen@realdolmen.com>
2020-11-06 08:40:08 +0100

    @qualifier used on constructor, setter and field so that controller knows which service of the 3 services needs to be injected

--- src/main/java/com/realdolmen/sfgdi/controllers/SetterInjectedController.java
+++ src/main/java/com/realdolmen/sfgdi/controllers/SetterInjectedController.java
@@ -3,6 +3,7 @@ package com.realdolmen.sfgdi.controllers;
 
 import com.realdolmen.sfgdi.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -11,7 +12,7 @@ public class SetterInjectedController {
     private GreetingService greetingService;
 
     @Autowired
-    public void setGreetingService(GreetingService greetingService) {
+    public void setGreetingService(@Qualifier("setterInjectedGreetingService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
--- src/test/java/com/realdolmen/sfgdi/controllers/PropertyInjectedControllerTest.java
+++ src/test/java/com/realdolmen/sfgdi/controllers/PropertyInjectedControllerTest.java
@@ -1,6 +1,6 @@
 package com.realdolmen.sfgdi.controllers;
 
-import com.realdolmen.sfgdi.services.GreetingServiceImpl;
+import com.realdolmen.sfgdi.services.ConstructorGreetingService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -11,7 +11,7 @@ public class PropertyInjectedControllerTest {
     @BeforeEach
     void setUp(){
         controller = new PropertyInjectedController();
-        controller.greetingService = new GreetingServiceImpl();
+        controller.greetingService = new ConstructorGreetingService();
     }
 
     @Test
